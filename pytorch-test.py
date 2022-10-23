139eccd6aae94ac5e4f56749e82588613947976f
schleising <stephen@schleising.net>
2022-10-23 17:09:19 +0100

    Move tensor to GPU using Metal

--- pytorch-test.py
+++ pytorch-test.py
@@ -3,3 +3,12 @@ import torch
 x = torch.rand(5, 3)
 
 print(x)
+
+print(torch.backends.mps.is_available()) # type: ignore
+print(torch.backends.mps.is_built()) # type: ignore
+
+print(x.device)
+
+y = x.to(device='mps')
+
+print(y.device)
